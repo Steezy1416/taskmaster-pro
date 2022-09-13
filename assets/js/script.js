536dd691ee2ae94df375a08a3d3e7beaf9b7c010
alexfortin <fortin.alex14@gmail.com>
2022-09-13 12:27:00 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,12 +54,8 @@ var auditTask = function (taskEl) {
     .text()
     .trim();
 
-  console.log(date);
-
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
-
-  console.log(time);
 
   // remove any old classes from element
   $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
@@ -71,6 +67,8 @@ var auditTask = function (taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+
+  console.log(taskEl)
 };
 
 // enable draggable/sortable feature on list-group elements
@@ -82,15 +80,23 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function (event, ui) {
     console.log(ui);
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag")
   },
   deactivate: function (event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function (event) {
     console.log(event);
+    $(event.target).addClass("dropover-active")
+    $(".bottom-trash").addClass("bottom-trash-active")
   },
   out: function (event) {
     console.log(event);
+    $(event.target).removeClass("dropover-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
   },
   update: function () {
     var tempArr = [];
@@ -161,7 +167,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -288,3 +294,9 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el)
+  })
+},1800000)
